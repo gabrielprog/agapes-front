d2cad7e52a6353bb0d1c40d4f02fe60fba73a854
Gabriel <gabrielprogramado@gmail.com>
2024-03-25 15:23:31 -0300

    Project init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/colors.css";
+import "./assets/css/global.css";
+
+import { ToastContainer } from "react-toastify";
+import Routers from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routers />
+      <ToastContainer />
+    </>
   );
 }
 
